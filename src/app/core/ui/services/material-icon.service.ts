e4b4ee12130625434057e36d9e1e3e22a06783e8
Florian Schwanz <florian.schwanz@interoberlin.de>
2022-04-25 00:12:23 +0200

    chore: add sport icons

--- src/app/core/ui/services/material-icon.service.ts
+++ src/app/core/ui/services/material-icon.service.ts
@@ -3,82 +3,47 @@ import {DomSanitizer} from '@angular/platform-browser';
 import {MatIconRegistry} from '@angular/material/icon';
 
 /**
- * Represents icon topic subdirectory
- */
-enum IconTopic {
-    ACTION = 'action',
-    ALERT = 'alert',
-    AV = 'av',
-    CONTENT = 'content',
-    COMMUNICATION = 'communication',
-    DEVICE = 'device',
-    EDITOR = 'editor',
-    FILE = 'file',
-    HARDWARE = 'hardware',
-    IMAGE = 'image',
-    MAPS = 'maps',
-    NAVIGATION = 'navigation',
-    SOCIAL = 'social'
-}
-
-/**
- * Represents a material design icon
- */
-class Icon {
-    /** Topic */
-    topic: IconTopic;
-    /** Name */
-    name: string;
-    /** File */
-    file: string;
-
-    /**
-     * Constructor
-     * @param topic topic
-     * @param name icon name
-     * @param file icon file name
-     */
-    constructor(topic: IconTopic, name: string, file: string) {
-        this.topic = topic;
-        this.name = name;
-        this.file = file;
-    }
-}
-
-/**
  * Handles Material icons
  */
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class MaterialIconService {
 
-    /** Root directory of material design icons */
-    private ICON_ROOT_DIR = 'assets/material-design-icons';
-    /** Icon variant */
-    private VARIANT = 'production';
-    /** List of icons */
-    private icons: Icon[] = [
-        // {topic: IconTopic.ACTION, name: 'search', file: 'ic_search_24px.svg'},
-        // {topic: IconTopic.CONTENT, name: 'filter_list', file: 'ic_filter_list_24px.svg'},
-        // {topic: IconTopic.IMAGE, name: 'flash_on', file: 'ic_flash_on_24px.svg'},
-        // {topic: IconTopic.IMAGE, name: 'nature', file: 'ic_nature_24px.svg'},
-        // {topic: IconTopic.IMAGE, name: 'nature_people', file: 'ic_nature_people_24px.svg'},
-        // {topic: IconTopic.MAPS, name: 'local_dining', file: 'ic_local_dining_24px.svg'}
-    ];
-
-    /**
-     * Initializes icons
-     *
-     * @param iconRegistry icon registry
-     * @param sanitizer sanitizer
-     */
-    public initializeIcons(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
-        this.icons.forEach(icon => {
-            iconRegistry.addSvgIcon(icon.name,
-                sanitizer.bypassSecurityTrustResourceUrl(this.ICON_ROOT_DIR + '/' + icon.topic + '/svg/' + this.VARIANT + '/' + icon.file));
-        });
-        iconRegistry.addSvgIcon('basketball', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/basketball.svg'));
-        iconRegistry.addSvgIcon('soccer', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/soccer.svg'));
-    }
+  /**
+   * Initializes icons
+   *
+   * @param iconRegistry icon registry
+   * @param sanitizer sanitizer
+   */
+  public initializeIcons(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon('badminton', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/badminton.svg'));
+    iconRegistry.addSvgIcon('baseball', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/baseball.svg'));
+    iconRegistry.addSvgIcon('basketball', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/basketball.svg'));
+    iconRegistry.addSvgIcon('biathlon', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/biathlon.svg'));
+    iconRegistry.addSvgIcon('bike', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/bike.svg'));
+    iconRegistry.addSvgIcon('boxing-glove', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/boxing-glove.svg'));
+    iconRegistry.addSvgIcon('carabiner', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/carabiner.svg'));
+    iconRegistry.addSvgIcon('cricket', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/cricket.svg'));
+    iconRegistry.addSvgIcon('curling', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/curling.svg'));
+    iconRegistry.addSvgIcon('fencing', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/fencing.svg'));
+    iconRegistry.addSvgIcon('football', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/football.svg'));
+    iconRegistry.addSvgIcon('golf', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/golf.svg'));
+    iconRegistry.addSvgIcon('handball', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/handball.svg'));
+    iconRegistry.addSvgIcon('hockey-sticks', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/hockey-sticks.svg'));
+    iconRegistry.addSvgIcon('karate', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/karate.svg'));
+    iconRegistry.addSvgIcon('kayaking', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/kayaking.svg'));
+    iconRegistry.addSvgIcon('racquetball', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/racquetball.svg'));
+    iconRegistry.addSvgIcon('rowing', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/rowing.svg'));
+    iconRegistry.addSvgIcon('rugby', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/rugby.svg'));
+    iconRegistry.addSvgIcon('run', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/run.svg'));
+    iconRegistry.addSvgIcon('skate', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/skate.svg'));
+    iconRegistry.addSvgIcon('skateboarding', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/skateboarding.svg'));
+    iconRegistry.addSvgIcon('soccer', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/soccer.svg'));
+    iconRegistry.addSvgIcon('swim', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/swim.svg'));
+    iconRegistry.addSvgIcon('table-tennis', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/table-tennis.svg'));
+    iconRegistry.addSvgIcon('tennis', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/tennis.svg'));
+    iconRegistry.addSvgIcon('volleyball', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/volleyball.svg'));
+    iconRegistry.addSvgIcon('yoga', sanitizer.bypassSecurityTrustResourceUrl('assets/material-icons/yoga.svg'));
+  }
 }
